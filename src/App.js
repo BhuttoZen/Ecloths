61dfc9fda0473a78c6dd8d60c0fd238645029aff
Zain UL Abdin <bhuttozen@gmail.com>
2022-07-31 11:44:10 +0500

    shop page + styles

--- src/App.js
+++ src/App.js
@@ -5,19 +5,16 @@ import { Route , Routes } from 'react-router-dom';
 
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 
-const HatsPage = () => {
-   return <h1>HatsPage</h1>
-  
-}
 
 function App() {
   return (
     <div>
       <Routes>
       <Route exact path='/' element={<HomePage />} />
-      <Route  path='/hats' element = {<HatsPage />} />
+      <Route  path='/shop' element = {<ShopPage />} />
 
       </Routes>
 
